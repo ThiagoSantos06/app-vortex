ce3ba8b298a17b05fad00a55b6c215fe94f8f225
Thiago <thiagohenrique1983c@gmail.com>
2025-12-05 11:54:38 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'vortex',
+  webDir: 'www'
+};
+
+export default config;
